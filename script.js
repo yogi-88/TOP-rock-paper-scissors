b625624e32425e058a89e5feb23a1891f12a77f1
rajeshwar singh bisht <ynegi6@gmail.com>
2023-08-10 22:04:36 +0100

    alert prompt working showing player chice, player and computer choice,and player score

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
+let playerScore = 0;
 // function that will randomly generate rock, paper, scissors for Computer
 function getComputerChoice () {
     let choices = ['rock', 'paper','scissors'];
     return(choices[Math.floor(Math.random() * choices.length)])
 }
 
-getComputerChoice();
-
 // function to play a single round of game between Computer and player
 function playRound (playerSelection, computerSelection) {
+    
+   
     if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection ==='paper' && computerSelection === 'rock') 
     || (playerSelection ==='scissors' && computerSelection === 'paper')){
+        playerScore ++; // Increment playerScore
         return ("Player wins");
+        
     } else if ((playerSelection ==='rock' && computerSelection === 'paper') || (playerSelection =='paper' && computerSelection === 'scissors') 
     || (playerSelection =='scissors' && computerSelection == 'rock')) {
-        return ("Player loses")
+        playerScore --; // Decrement playerScore
+        return ("Player loses");
+        
     } else {
-        return ("There is a tie")
+        playerScore = 0; //Reset playerScore
+        return ("There is a tie");
+        
     }
+
 }
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(`playerSelection:  ${playerSelection}, computerSelection: ${computerSelection}`)
-// console.log(playRound(playerSelection, computerSelection))
+
 
 // function game toplay a 5 round game that keeps score and reports a winner or loser at the end.
 function game() {
     let roundsPlayed =0;
-    while (roundsPlayed <= 5){
+    
+    while (roundsPlayed < 5){
         let playerSelection = prompt("Please enter your selection from Rock Paper Scissors.");
         let computerSelection = getComputerChoice();
-        console.log(`${playerSelection}: ${computerSelection}`);console.log(`playerSelection:  ${playerSelection}, computerSelection: ${computerSelection}`)
-        playRound(playerSelection,computerSelection)
+        alert(`playerSelection:  ${playerSelection}, computerSelection: ${computerSelection}`);
+        let result = playRound(playerSelection, computerSelection);
+        alert(result);
+
         roundsPlayed += 1
     }
+    alert('You won with '+ playerScore +' points.')
 }
 game();
 
