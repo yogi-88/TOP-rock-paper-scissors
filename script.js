f78267f56e3b8e9e197070fd2241dd5da7bc5a21
rajeshwar singh bisht <ynegi6@gmail.com>
2023-08-10 21:14:56 +0100

    playround function added which helps in finding winner

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // function that will randomly generate rock, paper, scissors for Computer
 function getComputerChoice () {
     let choices = ['rock', 'paper','scissors'];
-    console.log(choices[Math.floor(Math.random() * choices.length)])
+    return(choices[Math.floor(Math.random() * choices.length)])
 }
 
 getComputerChoice();
+
+// function to play a single round of game between Computer and player
+function playRound (playerSelection, computerSelection) {
+    if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection ==='paper' && computerSelection === 'rock') 
+    || (playerSelection ==='scissors' && computerSelection === 'paper')){
+        return ("Player wins");
+    } else if ((playerSelection ==='rock' && computerSelection === 'paper') || (playerSelection =='paper' && computerSelection === 'scissors') 
+    || (playerSelection =='scissors' && computerSelection == 'rock')) {
+        return ("Player loses")
+    } else {
+        return ("There is a tie")
+    }
+}
+    
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(`playerSelection:  ${playerSelection}, computerSelection: ${computerSelection}`)
+console.log(playRound(playerSelection, computerSelection))
