5a2b91353fda7bfb52a3f2247d10bab308e5619a
rajeshwar singh bisht <ynegi6@gmail.com>
2023-08-10 21:28:50 +0100

    function game added which prompts 5 round functionality working

--- script.js
+++ script.js
@@ -18,10 +18,23 @@ function playRound (playerSelection, computerSelection) {
         return ("There is a tie")
     }
 }
-    
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(`playerSelection:  ${playerSelection}, computerSelection: ${computerSelection}`)
+// console.log(playRound(playerSelection, computerSelection))
+
+// function game toplay a 5 round game that keeps score and reports a winner or loser at the end.
+function game() {
+    let roundsPlayed =0;
+    while (roundsPlayed <= 5){
+        let playerSelection = prompt("Please enter your selection from Rock Paper Scissors.");
+        let computerSelection = getComputerChoice();
+        console.log(`${playerSelection}: ${computerSelection}`);console.log(`playerSelection:  ${playerSelection}, computerSelection: ${computerSelection}`)
+        playRound(playerSelection,computerSelection)
+        roundsPlayed += 1
+    }
+}
+game();
 
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(`playerSelection:  ${playerSelection}, computerSelection: ${computerSelection}`)
-console.log(playRound(playerSelection, computerSelection))
